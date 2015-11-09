be3359ea4784591d1619b1f98a70d03e7ab7e0c1
Greg Hale <imalsogreg@gmail.com>
2015-11-09 17:33:52 -0500

    move ffi to main module

--- src/Data/Yaml.hs
+++ src/Data/Yaml.hs
@@ -1,19 +1,29 @@
-{-# LANGUAGE RankNTypes, FlexibleContexts, OverloadedStrings #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE JavaScriptFFI #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE CPP #-}
+
 module Data.Yaml where
 
 import qualified Data.Aeson as A
 import qualified Data.Text  as T
-import           Data.Yaml.Internal (y_load)
 import           GHCJS.Marshal
 import           GHCJS.Foreign
 import           GHCJS.Types
 
 decodeYaml :: A.FromJSON a => T.Text -> IO (Maybe a)
-decodeYaml t =
+decodeYaml t = do
   let x = y_load (toJSString t)
-      y = fromJSRef x
-  in  y
+  y <- fromJSRef x
+  case A.fromJSON <$> y of
+    Just (A.Success v) -> return v
+    _                  -> return Nothing
 
-y :: Int
-y = let p = decodeYaml "Hello" :: Bool
-    in  1
+
+#ifdef __GHCJS__
+foreign import javascript unsafe "load($1)" y_load :: JSString -> JSRef A.Value
+#else
+y_load = error "y_load only supported by ghcjs"
+#endif
+
--- src/Data/Yaml/Internal.hs
+++ src/Data/Yaml/Internal.hs
@@ -3,11 +3,10 @@
 module Data.Yaml.Internal where
 
 import qualified Data.Aeson as A
-import GHCJS.Foreign
 import GHCJS.Types
 
 #ifdef __GHCJS__
-foreign import javascript unsafe "load($1)" y_load :: A.FromJSON a => JSString -> JSObject a
+foreign import javascript unsafe "load($1)" y_load :: JSString -> JSRef A.Value
 #else
 y_load = error "y_load only supported by ghcjs"
 #endif
