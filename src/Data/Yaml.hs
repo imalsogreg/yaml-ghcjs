21eb80ffaa3afd68b50a328e6eec474d96d8a9e7
Greg Hale <imalsogreg@gmail.com>
2015-11-21 20:27:21 -0500

    Working test on ghcjs improved base

--- src/Data/Yaml.hs
+++ src/Data/Yaml.hs
@@ -6,23 +6,23 @@
 
 module Data.Yaml where
 
-import qualified Data.Aeson as A
-import qualified Data.Text  as T
+import qualified Data.Aeson    as A
+import qualified Data.JSString as JSS
+import qualified Data.Text     as T
 import           GHCJS.Marshal
-import           GHCJS.Foreign
 import           GHCJS.Types
 
 decodeYaml :: A.FromJSON a => T.Text -> IO (Maybe a)
 decodeYaml t = do
-  let x = y_load (toJSString t)
-  y <- fromJSRef x
+  let x = y_load (JSS.pack $ T.unpack $ t)
+  y <- fromJSVal x
   case A.fromJSON <$> y of
     Just (A.Success v) -> return v
     _                  -> return Nothing
 
 
 #ifdef __GHCJS__
-foreign import javascript unsafe "jsyaml.load($1)" y_load :: JSString -> JSRef A.Value
+foreign import javascript unsafe "jsyaml.load($1)" y_load :: JSString -> JSVal
 #else
 y_load = error "y_load only supported by ghcjs"
 #endif
